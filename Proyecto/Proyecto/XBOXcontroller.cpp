398620c4928dc73d1ab99eaa4cff9c222393ec67
Alberto Cavadia <osoflash_2@hotmail.com>
2014-06-02 23:09:24 -0430

    Clases de texto y control XBOX

---
+++ Proyecto/Proyecto/XBOXcontroller.cpp
@@ -0,0 +1,30 @@
+#include "XBOXcontroller.h"
+
+XBOXcontroller::XBOXcontroller(int playerNumber){
+	_controllerNum = playerNumber;
+}
+
+XINPUT_STATE XBOXcontroller:: getState(){
+	ZeroMemory(&_controllerState, sizeof(XINPUT_STATE));
+	XInputGetState(_controllerNum, &_controllerState);
+	return _controllerState;
+}
+
+bool XBOXcontroller:: isConnected(){
+	ZeroMemory(&_controllerState, sizeof(XINPUT_STATE));
+	DWORD Result = XInputGetState(_controllerNum, &_controllerState);
+
+	if(Result == ERROR_SUCCESS)
+		return true;
+	else
+		return false;
+	
+}
+
+void XBOXcontroller:: vibrate(int leftVal, int rightVal){
+	XINPUT_VIBRATION Vibration;
+	ZeroMemory(&Vibration, sizeof(XINPUT_VIBRATION));
+	Vibration.wLeftMotorSpeed = leftVal;
+	Vibration.wRightMotorSpeed = rightVal;
+	XInputSetState(_controllerNum, &Vibration);
+}
